0a5b01f4c200c3e92ac1cdef62a465415cd8fe27
valeriagamiz <Valeria@Valerias-MacBook-Pro.local>
2016-04-05 03:00:45 -0400

    js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var $svg = $('svg');
+var $lines = $('.lines');
+var $btn = $('.btn');
+var $engage = $('.engage');
+
+
+
+$btn.on('click', function() {
+$lines.addClass('linify');
+$lines.removeClass('lines');
+$engage.addClass('is-engaged');
+});
+
+$btn.on('mouseenter', function () {
+  $(this).children('path:last-child').css('fill', '#f60');
+});
